85a610c4b03cb3b168ffddb44caff4348586bc00
metrics21 <dnflskfk8665@>
2024-04-29 10:41:01 +0900

    [feature/gmae] 27 -

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -4,6 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    @Test
+    public void createGame(){
+        Game game = new Game();
+        assertNotNull(game);
+    }
 
+    @Test
+    public void throwExceptionWhenInputIsNull(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
 
 }
